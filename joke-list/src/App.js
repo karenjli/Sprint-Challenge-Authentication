d3df462af65e35a22f45bb7fafca44299e98d971
karenjli <karenjy.li@gmail.com>
2019-10-18 13:03:26 -0500

    attempted stretch goal

---
+++ joke-list/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//component
+import Register from "./components/register";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
